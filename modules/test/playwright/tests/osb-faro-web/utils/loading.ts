4e41b6bd483e94a717327477bfe81224ecf6f6f6
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-06-20 17:16:29 -0300

    WIP Marcinho amostradinho

--- modules/test/playwright/tests/osb-faro-web/utils/loading.ts
+++ modules/test/playwright/tests/osb-faro-web/utils/loading.ts
@@ -1,7 +1,9 @@
+import {Locator, Page} from '@playwright/test';
+
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-export async function waitForLoading(page) {
+export async function waitForLoading(page: Page | Locator) {
 	await page.locator('.loading-animation').waitFor({state: 'detached'});
 }
